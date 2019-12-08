2c213f58daa1caee48c99fa21656f9df2cab8528
batablo <mutemath.us22@gmail.com>
2019-12-08 16:47:51 +0900

    [add]propsの設定

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 15 },
+    { name: "NoName"}
+  ]
   return (
-    <div><Cat /></div>
+    <div>
+      {
+        profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+return <div>私は{props.name}です！年齢は{props.age}歳です！</div>
+}
+
+//プロパティの初期値を設定
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
