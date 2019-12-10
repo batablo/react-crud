5311788228d378c0623b9f75377cdcba8eb000fd
batablo <mutemath.us22@gmail.com>
2019-12-11 00:16:50 +0900

    [add]reducerとcomponentの処理を追加

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,15 @@
+import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
 // actionの種類によって処理を分岐
 export default (state = {}, action) => {
   switch (action.type) {
-    case READ_EVENTS: 
-      return state
+    case READ_EVENTS:
+      // {
+      //   1: {"id": 1, "title": ...}
+      // }
+      // 上の形でstateに入れる
+      return _.mapKeys(action.response.data, 'id')
     default:
       return state
   }
